eeb19247154462076c38cf2452296f73640389f7
LuuTanDat27 <115093574+LuuTanDat27@users.noreply.github.com>
2024-04-05 01:02:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ loginBtn.addEventListener('click', () => {
     // Kiểm tra username và password
     if (username === "luutandat@gmail.com" && password === "290907") {
         // Nếu đúng, chuyển hướng đến trang chủ
-        window.location.href = "URL của trang chủ";
+        window.location.href = "https://dailysieure.com";
     } else {
         // Nếu sai, hiển thị thông báo lỗi hoặc thực hiện các hành động khác
         alert("Invalid username or password. Please try again.");
